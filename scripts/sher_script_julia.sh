c44e0d6b7b2fa6d268a4858e260ca8d2fb41eeda
Julia Costacurta <jcostacurta11@gmail.com>
2024-01-08 15:57:38 -0800

    sherlock script

---
+++ scripts/sher_script_julia.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=nmrnn
+#SBATCH --partition=normal,hns, swl1
+#SBATCH --time=4:0:0
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
+
+# activate virtual environment
+source /home/groups/swl1/nm-rnn/.venv/bin/activate
+
+# run the sweep!
+wandb agent nm-rnn/nm-rnn-mwg/a761y26q
