b5e29ea0543bde8e58d7066e78f950c8bbb72a7d
Julia Costacurta <jcostacurta11@gmail.com>
2024-02-05 14:56:00 -0800

    adding lr only training

--- nmrnn/fitting.py
+++ nmrnn/fitting.py
@@ -102,6 +102,8 @@ def fit_mwg_lr_rnn(inputs, targets, loss_masks, params, optimizer, x0, num_iters
 
     return params, losses
 
+
+# training function to fit only neuromodulatory parameters
 def fit_mwg_nm_only(inputs, targets, loss_masks, nm_params, other_params, optimizer, x0, z0, num_iters, tau_x, tau_z,
                     plots=True, wandb_log=False, final_wandb_plot=False): # training on full set of data
     opt_state = optimizer.init(nm_params)
@@ -127,6 +129,64 @@ def fit_mwg_nm_only(inputs, targets, loss_masks, nm_params, other_params, optimi
         if wandb_log: wandb.log({'loss':loss_values[-1]})
 
         params = dict(nm_params, **other_params)
+        ys, _, zs = nm_rnn(params, x0, z0, sample_inputs, tau_x, tau_z)
+
+        if plots:
+            # plt.figure(figsize=[10,6])
+            fig, (ax0, ax1) = plt.subplots(2, 1, figsize=[10,6])
+            # ax0.xlabel('Timestep')
+            ax0.plot(sample_targets, label='True target')
+            ax0.plot(ys, label='RNN target')
+            ax0.legend()
+            ax1.set_xlabel('Timestep')
+            m = params['nm_sigmoid_weight']
+            b = params['nm_sigmoid_intercept']
+            ax1.plot(jax.nn.sigmoid((zs @ m.T + b)))
+            # ax1.legend()
+            plt.pause(0.1)
+
+    if final_wandb_plot:
+        # plt.figure(figsize=[10,6])
+        fig, (ax0, ax1) = plt.subplots(2, 1, figsize=[10, 6])
+        # ax0.xlabel('Timestep')
+        ax0.set_title('after training nm only')
+        ax0.plot(sample_targets, label='True target')
+        ax0.plot(ys, label='RNN target')
+        ax0.legend()
+        ax1.set_xlabel('Timestep')
+        m = params['nm_sigmoid_weight']
+        b = params['nm_sigmoid_intercept']
+        ax1.plot(jax.nn.sigmoid((zs @ m.T + b)))
+        wandb.log({'nm_only_curves':wandb.Image(fig)}, commit=True)
+
+    return params, losses
+
+# training function to fit only low-rank parameters 
+def fit_mwg_lr_only(inputs, targets, loss_masks, nm_params, lr_params, optimizer, x0, z0, num_iters, tau_x, tau_z,
+                    plots=False, wandb_log=False, final_wandb_plot=False): # training on full set of data
+    opt_state = optimizer.init(nm_params)
+    N_data = inputs.shape[0]
+
+    @jit
+    def _step(params_and_opt, input):
+        (nm_params, opt_state) = params_and_opt
+        #pdb.set_breakpoint()
+        loss_value, grads = jax.value_and_grad(batched_nm_rnn_loss_frozen)(nm_params, lr_params, x0, z0, inputs, tau_x, tau_z, targets, loss_masks)
+        updates, opt_state = optimizer.update(grads, opt_state, lr_params)
+        nm_params = optax.apply_updates(lr_params, updates)
+        return (lr_params, opt_state), (lr_params, loss_value)
+
+    losses = []
+    sample_inputs, sample_targets, sample_masks = inputs[0], targets[0], loss_masks[0]  # grab a single trial to plot output
+    for n in range(num_iters//1000):
+        # (params, opt_state), loss_value = _step((params, opt_state))
+        (lr_params,_), (_, loss_values) = lax.scan(_step, (lr_params, opt_state), None, length=1000) #arange bc the inputs aren't changing
+        losses.append(loss_values)
+        # if n % 100 == 0:
+        print(f'step {(n+1)*1000}, loss: {loss_values[-1]}')
+        if wandb_log: wandb.log({'loss':loss_values[-1]})
+
+        params = dict(nm_params, **lr_params)
         ys, _, zs = nm_rnn(params, x0, z0, sample_inputs, tau_x, tau_z)
 
         if plots:
