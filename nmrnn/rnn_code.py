774c039efa4bc256b2663f6f5e6ccd76f89ce92c
Julia Costacurta <jcostacurta11@gmail.com>
2024-04-03 20:45:39 -0700

    created nm-context-only rnn, added arg for orthogonalizing U or not

--- nmrnn/rnn_code.py
+++ nmrnn/rnn_code.py
@@ -7,7 +7,7 @@ import optax
 import matplotlib.pyplot as plt
 
 # CODE FOR NM-RNN
-def nm_rnn(params, x0, z0, inputs, tau_x, tau_z, nln=jnp.tanh):
+def nm_rnn(params, x0, z0, inputs, tau_x, tau_z, nln=jnp.tanh, orth_u=True):
     """
     Arguments:
     - params
@@ -17,7 +17,7 @@ def nm_rnn(params, x0, z0, inputs, tau_x, tau_z, nln=jnp.tanh):
     """
 
     U = params["row_factors"]       # D x R
-    U, _ = jnp.linalg.qr(U)         # orthogonalize
+    if orth_u: U, _ = jnp.linalg.qr(U)         # orthogonalize
     V = params["column_factors"]    # D x R
     # V, _ = jnp.linalg.qr(V)         # orthogonalize
     B_xu = params["input_weights"]     # D x M
@@ -100,7 +100,6 @@ def batched_nm_rnn_loss_frozen(nm_params, other_params, x0, z0, inputs, tau_x, t
 
 
 # CODE FOR LINEAR SYMMETRIC NM-RNN
-
 def lin_sym_nm_rnn(params, x0, z0, inputs, tau_x, tau_z):
     """
     Arguments:
@@ -149,4 +148,62 @@ batched_lin_sym_nm_rnn = vmap(lin_sym_nm_rnn, in_axes=(None, None, None, 0, None
 
 def batched_lin_sym_nm_rnn_loss(params, x0, z0, batch_inputs, tau_x, tau_z, batch_targets, batch_mask):
     ys, _, _ = batched_lin_sym_nm_rnn(params, x0, z0, batch_inputs, tau_x, tau_z)
+    return jnp.sum(((ys - batch_targets)**2)*batch_mask)/jnp.sum(batch_mask)
+
+# CODE FOR MULTITASK NM-RNN WHERE ONLY NM RECEIVES CONTEXT
+def context_nm_rnn(params, x0, z0, task_inputs, context_inputs, tau_x, tau_z, nln=jnp.tanh):
+    """
+    Arguments:
+    - params
+    - x0
+    - task_inputs
+    - context_inputs
+    - tau   : decay constant
+    """
+
+    U = params["row_factors"]       # D x R
+    U, _ = jnp.linalg.qr(U)         # orthogonalize
+    V = params["column_factors"]    # D x R
+    # V, _ = jnp.linalg.qr(V)         # orthogonalize
+    B_xu = params["input_weights"]     # D x M
+    C = params["readout_weights"]   # O x D
+    W_zz = params["nm_rec_weight"]         # dim_nm x dim_nm
+    B_zu = params["nm_input_weight"]      # dim_nm x (M + dim_context)
+    m = params["nm_sigmoid_weight"]         # scalar
+    c = params["nm_sigmoid_intercept"]      # scalar
+
+    N = x0.shape[0]
+
+    inputs_x = task_inputs # T x M
+    #TODO: check concat
+    inputs_z = jnp.concatenate((task_inputs, context_inputs), axis=-1) # T x (M + dim_context)
+
+    def _step(x_and_z, u_x, u_z):
+        x, z = x_and_z
+
+        # update z
+        z = (1.0 - (1. / tau_z)) * z + (1. / tau_z) * W_zz @ nln(z)
+        z += (1. / tau_z) * B_zu @ u_z
+
+        # update x
+        xp = x # hold onto previous value
+        s = jax.nn.sigmoid(m @ z + c) # calculate nm signal
+        x = (1.0 - (1. / tau_x)) * xp # decay term
+        h = V.T @ nln(xp)
+        #x += (1. / (tau_x * N)) * (U * s) @ h  # divide by N
+        x += (1. / (tau_x)) * (U * s) @ h # don't divide by N now because we normalized U
+        x += (1. / tau_x) * B_xu @ u_x
+
+        # calculate y
+        y = C @ x
+        return (x, z), (y, x, z)
+
+    _, (ys, xs, zs) = lax.scan(_step, (x0, z0), inputs_x, inputs_z)
+
+    return ys, xs, zs
+
+batched_context_nm_rnn = vmap(context_nm_rnn, in_axes=(None, None, None, 0, 0, None, None))
+
+def batched_context_nm_rnn_loss(params, x0, z0, batch_task_inputs, batch_context_inputs, tau_x, tau_z, batch_targets, batch_mask):
+    ys, _, _ = batched_context_nm_rnn(params, x0, z0, batch_task_inputs, batch_context_inputs, tau_x, tau_z)
     return jnp.sum(((ys - batch_targets)**2)*batch_mask)/jnp.sum(batch_mask)
