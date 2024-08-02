414425205bde1d7b16400aecff2d4fd951a5f31e
Julia Costacurta <jcostacurta11@gmail.com>
2024-08-01 17:03:27 -0700

    vanilla rnn mwg

--- scripts/train_rnn_mwg.py
+++ scripts/train_rnn_mwg.py
@@ -9,16 +9,20 @@ import matplotlib.pyplot as plt
 import wandb
 
 from nmrnn.data_generation import sample_all
-from nmrnn.util import random_lstm_params, log_wandb_model
-from nmrnn.fitting import fit_lstm_mwg
-from nmrnn.rnn_code import batched_simple_lstm, initialize_carry
+from nmrnn.util import random_rnn_params, log_wandb_model
+from nmrnn.fitting import fit_rnn_mwg
+from nmrnn.rnn_code import batched_rnn
 
 # parameters we want to track in wandb
 default_config = dict(
     # model parameters
-    N = 100,    # hidden size
-    U = 3,      # input size
-    O = 1,      # output size
+    N = 100,    # hidden state dim
+    U = 3,      # input dim
+    O = 1,      # output dimension
+    # got rid of K for now, set to R by default
+    #K = 2,      # NM sigmoid dimension (must be 1 or R)
+    # Model Hyperparameters
+    tau = 10,
     # Timing (task) parameters
     dt = 10,#ms
     # Data Generation
@@ -32,7 +36,7 @@ default_config = dict(
     keyind = 13,
 )
 
-projectname = "lstm-mwg"
+projectname = "vanilla-rnn-mwg"
 wandb.init(config=default_config, project=projectname, entity='nm-rnn')
 config = wandb.config
 
@@ -51,16 +55,18 @@ optimizer = optax.chain(
   optax.adamw(learning_rate=1e-2),
 )
 
-# generate random initial parameters
-init_params = random_lstm_params(key, config['U'], config['N'], config['O'])
-init_carry = initialize_carry(config['N'], key)
+x0 = jnp.ones((config['N'],))*0.1
 
-# train on all params
-params, losses = fit_lstm_mwg(all_inputs, all_outputs, init_params, optimizer, 
-                              init_carry, config['num_full_train_iters'], wandb_log=True)
+# generate random initial parameters
+init_params = random_rnn_params(key, config['U'], config['N'], config['O'])
+
+# train
+params, losses = fit_rnn_mwg(all_inputs, all_outputs, all_masks,
+                                init_params, optimizer, x0, config['num_full_train_iters'],
+                                config['tau'], wandb_log=True)
 
 # log model
-log_wandb_model(params, "lstm_n{}".format(config['N']), 'model')
+log_wandb_model(params, "rnn_n{}".format(config['N']), 'model')
 
 ################## more plots/metrics to analyze model generalization
 
@@ -76,8 +82,7 @@ new_inputs, new_outputs, new_masks = sample_all(config['T'],
             measure_max,
             config['delay'],)
 
-(c0, h0) = init_carry
-carrys, ys = batched_simple_lstm(params, c0, h0, new_inputs)
+ys, _ = batched_rnn(params, x0, new_inputs, config['tau'])
 
 ################## generalization plots
 N = new_intervals.shape[1]
@@ -85,15 +90,15 @@ N = new_intervals.shape[1]
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 5), sharey=True, sharex=True)
 for i in [4, 5, 6, 7]:
-    ax1.plot(new_outputs[i][new_masks[i].astype(bool)], 'k--', alpha=0.7)
-    ax1.plot(ys[i][new_masks[i].astype(bool)], c='tab:purple')
+    ax1.plot(new_outputs[i][new_masks[i,:,0].astype(bool)], 'k--', alpha=0.7)
+    ax1.plot(ys[i][new_masks[i,:,0].astype(bool)], c='tab:purple')
 
 for i in [0, 1, 2, 3]:
-    ax2.plot(new_outputs[i][new_masks[i].astype(bool)], 'k--', alpha=0.7)
-    ax2.plot(ys[i][new_masks[i].astype(bool)], c='tab:red')
+    ax2.plot(new_outputs[i][new_masks[i,:,0].astype(bool)], 'k--', alpha=0.7)
+    ax2.plot(ys[i][new_masks[i,:,0].astype(bool)], c='tab:red')
 
 for i in [8,9,10,11]:
-    ax2.plot(new_outputs[i][new_masks[i].astype(bool)], 'k--', alpha=0.7)
-    ax2.plot(ys[i][new_masks[i].astype(bool)], c='tab:blue')
+    ax2.plot(new_outputs[i][new_masks[i,:,0].astype(bool)], 'k--', alpha=0.7)
+    ax2.plot(ys[i][new_masks[i,:,0].astype(bool)], c='tab:blue')
 
 wandb.log({'generalization_output': wandb.Image(fig)}, commit=True)
