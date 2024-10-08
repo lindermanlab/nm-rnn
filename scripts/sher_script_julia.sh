921e9a6aa791dc0539b679131ec5aa2ceecbc422
Julia Christina Costacurta <jcostac@sh03-ln08.stanford.edu>
2024-10-08 09:25:23 -0700

    sherlock script

--- scripts/sher_script_julia.sh
+++ scripts/sher_script_julia.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=nmrnn
 #SBATCH --partition=normal,hns,owners,swl1
-#SBATCH --time=12:0:0
+#SBATCH --time=4:0:0
 #SBATCH --ntasks=1
 #SBATCH --mincpus=32
 #SBATCH --mem-per-cpu=1G
@@ -20,4 +20,8 @@ source /home/groups/swl1/nm-rnn/.venv/bin/activate
 #python3 train_multitask.py
 #wandb agent nm-rnn/nm-rnn-multitask/dpivmlbz
 #wandb agent nm-rnn/nm-rnn-multitask/14nzxzaj
-wandb agent nm-rnn/nm-rnn-multitask/m2cv1qxy
+#wandb agent nm-rnn/nm-rnn-multitask/m2cv1qxy
+#wandb agent nm-rnn/vanilla-rnn-mwg/j89w2gxe
+#wandb agent nm-rnn/multitask-lstm/q8u19ysl
+#wandb agent nm-rnn/multitask-lstm/5ewltj7a
+wandb agent nm-rnn/lstm-mwg/fj9jlfvf
